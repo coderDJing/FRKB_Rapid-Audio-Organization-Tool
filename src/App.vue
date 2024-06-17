0917d9fc421c64198abfce981fe9763090bb0e81
coderDJing <tongrenlu@foxmail.com>
2024-06-17 22:33:32 +0800

    引入图形化界面，提交部分进度

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <titleComponent />
+  <homePage />
+
+</template>
+
+<script setup>
+import homePage from "./pages/homePage.vue"
+import titleComponent from './components/titleComponent.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #cccccc;
+  background-color: #1f1f1f;
+  width: 100vw;
+  height: 100vh;
+}
+
+body {
+  margin: 0px;
+  background-color: #1f1f1f;
+}
+</style>
---
+++ src/components/titleComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import chromeClose from '@/assets/chrome-close.svg'
+</script>
+<template>
+    <div class="titleComponent">
+        <div>
+
+        </div>
+        <span>better music library for DJ</span>
+        <div>
+            <div>
+                <img :src="chromeClose">
+            </div>
+
+        </div>
+    </div>
+</template>
+<style lang="scss">
+.titleComponent {
+    width: 100vw;
+    height: 30px;
+    background-color: #2a2a2a;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 13px;
+    padding: 0px 10px;
+    box-sizing: border-box;
+}
+</style>
