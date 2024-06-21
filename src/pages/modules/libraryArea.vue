a768bfab0c4675489eb33ee00e6422f59666649c
coderDJing <tongrenlu@foxmail.com>
2024-06-21 14:27:50 +0800

    提交进度，完成左侧功能选择区

---
+++ src/pages/modules/libraryArea.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+<template>
+    <div style="height: 100%;width: 100%;display: flex;">
+        <div style="flex-grow: 1;background-color: #181818;"></div>
+        <div style="width:1px;background-color: #2b2b2b;"></div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
