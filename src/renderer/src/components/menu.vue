6d9bc879fd14dde2a26a3b657db8f03fbb5a9555
coderDJing <149645781+coderDJing@users.noreply.github.com>
2024-06-25 01:02:33 +0800

    上传进度

--- src/renderer/src/components/menu.vue
+++ src/renderer/src/components/menu.vue
@@ -1,16 +1,37 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, watch, defineEmits } from 'vue';
+const emit = defineEmits(['emitMenuEnd', 'menuButtonClick'])
 const props = defineProps({
   menuArr: {
     type: Array,
     required: true
+  },
+  show: {
+    type: Boolean,
+    required: true
   }
 })
+const documentHandleClick = () => {
+  document.removeEventListener('click', documentHandleClick)
+  emit('emitMenuEnd')
+}
+watch(() => props.show, () => {
+  if (props.show == true) {
+    document.addEventListener('click', documentHandleClick)
+  }
+})
+const menuButtonClick = (item) => {
+  emit('menuButtonClick', item)
+}
+
 </script>
 <template>
-  <div class="menu">
-    <div>
-      123
+  <div class="menu" v-if="props.show">
+    <div v-for="item of props.menuArr" class="menuGroup">
+      <div v-for="button of item" class="menuButton" @click="menuButtonClick(button)">
+        <span>{{ button.name }}</span>
+        <span>{{ button.shortcutKey }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -20,5 +41,29 @@ const props = defineProps({
   background-color: #1f1f1f;
   border: 1px solid #454545;
   font-size: 14px;
+  width: 250px;
+  border-radius: 5px;
+
+  .menuGroup {
+    border-bottom: 1px solid #454545;
+    padding: 5px 5px;
+
+    .menuButton {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px 20px;
+      border-radius: 5px;
+
+      &:hover {
+        background-color: #0078d4;
+        color: white;
+      }
+    }
+  }
+
+  .menuGroup:last-child {
+    border-bottom: 0px;
+  }
+
 }
 </style>
---
+++ src/renderer/src/components/scanNewSongDialog.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+<template>
+    <div class="dialog unselectable">
+        <div style="width: 450px;height: 260px;" class="inner">
+            <div style="text-align: center;height:30px;line-height: 30px;font-size: 14px;"><span>入库新歌曲</span></div>
+            <div style="padding-left: 20px;padding-top: 30px;padding-right: 20px;">
+                <div style="display: flex;">
+                    <div><span>选择文件夹：</span></div>
+                    <div style="flex-grow: 1;">
+                        <div style="width: 100%;height: 100%;background-color: #313131;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
