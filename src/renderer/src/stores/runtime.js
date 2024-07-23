ca9c6110e92481466da6864b6de5d4c865b6f0a5
coderDJing <tongrenlu@foxmail.com>
2024-07-23 17:14:35 +0800

    增加loading，增加导入时不能再执行导入，增加导入时不能关闭程序

--- src/renderer/src/stores/runtime.js
+++ src/renderer/src/stores/runtime.js
@@ -8,21 +8,23 @@ export const useRuntimeStore = defineStore('runtime', {
       activeMenuUUID: '',
       layoutConfig: {
         libraryAreaWidth: 200,
-        isMaxMainWin: false,//上次关闭前是否最大化
+        isMaxMainWin: false, //上次关闭前是否最大化
         scanNewSongDialog: {
           isDeleteSourceFile: true, //是否删除源文件
           isDeleteSourceDir: true, //是否删除文件夹，暂时注释，无法判断文件夹是否是桌面等重要文件夹
           isComparisonSongFingerprint: true, //比对声音指纹去重
           isPushSongFingerprintLibrary: true //是否加入声音指纹库
         },
-        mainWindowWidth: 900,//上次关闭前窗口化时的宽
-        mainWindowHeight: 600,//上次关闭前窗口化时的高
+        mainWindowWidth: 900, //上次关闭前窗口化时的宽
+        mainWindowHeight: 600 //上次关闭前窗口化时的高
       },
       dragItemData: null,
       libraryTree: {},
       selectSongListDialogShow: false,
       dialogSelectedSongListUUID: '', //dialog中被选中的歌单UUID
-      selectedSongListUUID: '' //被选中的歌单UUID
+      selectedSongListUUID: '', //被选中的歌单UUID
+      importingSongListUUID: '', //正在执行导入中的歌单
+      isProgressing: false //正在执行某计算或IO任务
     }
   }
 })
