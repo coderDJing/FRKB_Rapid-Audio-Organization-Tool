98dcae7786382f9ad722040b31c5d71dea900557
coderDJing <tongrenlu@foxmail.com>
2024-07-31 18:08:26 +0800

    封装bubbleBox，解决报错，上传playerControl进度

--- src/renderer/src/components/playerControls.vue
+++ src/renderer/src/components/playerControls.vue
@@ -6,9 +6,22 @@ import pause from '@renderer/assets/pause.png'
 import fastForward from '@renderer/assets/fastForward.png'
 import nextSong from '@renderer/assets/nextSong.png'
 import more from '@renderer/assets/more.png'
-import { ref, onUnmounted } from 'vue'
-const playing = ref(true)
+import { ref, onUnmounted, watch } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import { useRuntimeStore } from '@renderer/stores/runtime'
+import bubbleBox from '@renderer/components/bubbleBox.vue'
 
+const uuid = uuidv4()
+const runtime = useRuntimeStore()
+const playing = ref(true)
+watch(
+  () => runtime.activeMenuUUID,
+  (val) => {
+    if (val !== uuid) {
+      moreMenuShow.value = false
+    }
+  }
+)
 const emits = defineEmits([
   'pause',
   'play',
@@ -74,6 +87,24 @@ onUnmounted(() => {
 defineExpose({
   setPlayingValue
 })
+
+const moreMenuShow = ref(false)
+const handelMoreClick = () => {
+  if (moreMenuShow.value) {
+    runtime.activeMenuUUID = ''
+    moreMenuShow.value = false
+    return
+  }
+  runtime.activeMenuUUID = uuid
+  moreMenuShow.value = true
+}
+
+const previousSongRef = ref(null)
+const fastBackwardRef = ref(null)
+const playRef = ref(null)
+const pauseRef = ref(null)
+const fastForwardRef = ref(null)
+const nextSongRef = ref(null)
 </script>
 <template>
   <div
@@ -86,32 +117,85 @@ defineExpose({
       align-items: center;
     "
   >
-    <div @click="handlePreviousSong()">
+    <div ref="previousSongRef" class="buttonIcon" @click="handlePreviousSong()">
       <img :src="previousSong" draggable="false" />
     </div>
-    <div @mousedown="handleFastBackward()">
+    <bubbleBox :dom="previousSongRef" title="上一首" shortcut="W" :left="50" />
+    <div ref="fastBackwardRef" class="buttonIcon" @mousedown="handleFastBackward()">
       <img :src="fastBackward" draggable="false" />
     </div>
-    <div v-if="!playing" @click="handlePlay()">
+    <bubbleBox :dom="fastBackwardRef" title="快退" shortcut="A" :left="100" />
+    <div ref="playRef" class="buttonIcon" v-show="!playing" @click="handlePlay()">
       <img :src="play" draggable="false" />
     </div>
-    <div v-if="playing" @click="handlePause()">
+    <bubbleBox :dom="playRef" title="播放" shortcut="Space" :left="150" />
+    <div ref="pauseRef" class="buttonIcon" v-show="playing" @click="handlePause()">
       <img :src="pause" draggable="false" />
     </div>
-    <div @mousedown="handleFastForward()">
+    <bubbleBox :dom="pauseRef" title="暂停" shortcut="Space" :left="150" />
+    <div ref="fastForwardRef" class="buttonIcon" @mousedown="handleFastForward()">
       <img :src="fastForward" draggable="false" />
     </div>
-    <div @click="handleNextSong()">
+    <bubbleBox :dom="fastForwardRef" title="快进" shortcut="D" :left="200" />
+    <div ref="nextSongRef" class="buttonIcon" @click="handleNextSong()">
       <img :src="nextSong" draggable="false" />
     </div>
-    <div>
+    <bubbleBox :dom="nextSongRef" title="下一首" shortcut="S" :left="250" />
+    <div class="buttonIcon" @click.stop="handelMoreClick()">
       <img :src="more" draggable="false" />
     </div>
   </div>
+  <transition name="fade">
+    <!-- todo加上快捷键icon -->
+    <div class="moreMenu" v-if="moreMenuShow">
+      <div class="menuButton">
+        <span>导出</span>
+      </div>
+      <div class="menuButton"><span>移动到筛选库</span><span>Q</span></div>
+      <div class="menuButton"><span>移动到精选库</span><span>E</span></div>
+      <div class="menuButton"><span>删除曲目</span><span>F</span></div>
+    </div>
+  </transition>
 </template>
 <style lang="scss" scoped>
+.moreMenu {
+  width: 200px;
+  height: 120px;
+  background-color: #202020;
+  position: absolute;
+  border: 1px solid #424242;
+  border-radius: 3px;
+  z-index: 99;
+  bottom: 60px;
+  left: 250px;
+  padding: 5px 5px;
+  font-size: 14px;
+
+  .menuButton {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 20px;
+    border-radius: 5px;
+
+    &:hover {
+      background-color: #0078d4;
+      color: white;
+    }
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.15s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
 .playerControls {
-  div {
+  .buttonIcon {
     height: 40px;
     width: 40px;
     display: flex;
