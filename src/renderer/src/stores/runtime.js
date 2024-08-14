e193bc8aa6bbd3d054622dfb620fe27c0be10151
coderDJing <tongrenlu@foxmail.com>
2024-08-14 16:24:14 +0800

    选项记忆改为localstorage实现

--- src/renderer/src/stores/runtime.js
+++ src/renderer/src/stores/runtime.js
@@ -10,12 +10,6 @@ export const useRuntimeStore = defineStore('runtime', {
       layoutConfig: {
         libraryAreaWidth: 200,
         isMaxMainWin: false, //上次关闭前是否最大化
-        scanNewSongDialog: {
-          isDeleteSourceFile: true, //是否删除源文件
-          isDeleteSourceDir: true, //是否删除文件夹，暂时注释，无法判断文件夹是否是桌面等重要文件夹
-          isComparisonSongFingerprint: true, //比对声音指纹去重
-          isPushSongFingerprintLibrary: true //是否加入声音指纹库
-        },
         mainWindowWidth: 900, //上次关闭前窗口化时的宽
         mainWindowHeight: 600 //上次关闭前窗口化时的高
       },
