e2658c2358c384a60c86f906b2aad1bef1a8782d
coderDJing <tongrenlu@foxmail.com>
2024-09-04 18:33:57 +0800

    上传进度

--- src/renderer/src/DatabaseInit.vue
+++ src/renderer/src/DatabaseInit.vue
@@ -1,8 +1,34 @@
-<script setup></script>
+<script setup>
+import { t } from '@renderer/utils/translate.js'
+</script>
 
 <template>
   <div style="height: 100%; max-height: 100%; width: 100%; display: flex; flex-direction: column">
-    123
+    <div
+      style="text-align: center; height: 30px; line-height: 30px; font-size: 14px"
+      class="canDrag"
+    >
+      <span style="font-weight: bold" class="title unselectable">{{
+        t('首次启动请选择数据存储位置')
+      }}</span>
+    </div>
+    <div style="padding-left: 20px; padding-top: 30px; padding-right: 20px">
+      <div style="display: flex">
+        <div class="formLabel">
+          <span>{{ t('选择文件夹') }}：</span>
+        </div>
+        <div style="width: 300px">
+          <div
+            class="chooseDirDiv flashing-border"
+            @click="clickChooseDir()"
+            :title="folderPathDisplay"
+            :class="{ 'is-flashing': flashArea == 'folderPathVal' }"
+          >
+            {{ folderPathDisplay }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss">
@@ -19,5 +45,26 @@
 body {
   margin: 0px;
   background-color: #1f1f1f;
+}
+
+.chooseDirDiv {
+  width: calc(100% - 5px);
+  height: 100%;
+  background-color: #313131;
+  cursor: pointer;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-all;
+  white-space: nowrap;
+  width: calc(100% - 5px);
+  font-size: 14px;
+  padding-left: 5px;
+}
+
+.formLabel {
+  width: 110px;
+  min-width: 110px;
+  text-align: left;
+  font-size: 14px;
 }
 </style>
