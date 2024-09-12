2a8cf2db8cd7d786c60aeb103052b803f20cabde
coderDJing <tongrenlu@foxmail.com>
2024-09-12 18:05:02 +0800

    完成python重构改为socket，todo electron分析逻辑改造

--- src/main/store.js
+++ src/main/store.js
@@ -2,9 +2,11 @@ let songFingerprintList = [] //声音指纹列表
 let databaseDir = '' //数据库目录
 let layoutConfig = {} //界面布局config
 let settingConfig = {} //设置config
+let analyseSongPort = ''
 export default {
   songFingerprintList,
   databaseDir,
   layoutConfig,
-  settingConfig
+  settingConfig,
+  analyseSongPort
 }
--- src/main/url.js
+++ src/main/url.js
@@ -1,7 +1,7 @@
 import { app } from 'electron'
 import enUsUrl from '../renderer/src/language/enUS.json?commonjs-external&asset'
 import zhCNUrl from '../renderer/src/language/zhCN.json?commonjs-external&asset'
-import analyseSongFingerprintPyScriptUrl from '../../resources/pyScript/analyseSongFingerprint/analyseSongFingerprint.exe?commonjs-external&asset&asarUnpack'
+import analyseSongPyScriptUrl from '../../resources/pyScript/analyseSong/analyseSong.exe?commonjs-external&asset&asarUnpack'
 const path = require('path')
 let userDataDir = ''
 if (app.isPackaged) {
@@ -14,7 +14,7 @@ let settingConfigFileUrl = path.join(userDataDir, 'config', 'settingConfig.json'
 export default {
   enUsUrl,
   zhCNUrl,
-  analyseSongFingerprintPyScriptUrl,
+  analyseSongPyScriptUrl,
   userDataDir,
   layoutConfigFileUrl,
   settingConfigFileUrl
