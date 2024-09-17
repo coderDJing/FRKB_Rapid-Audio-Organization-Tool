a67bab365b9ba3d347b8e435516d1960b95a5144
coderDJing <149645781+coderDJing@users.noreply.github.com>
2024-09-18 01:10:13 +0800

    上传python后端重构为socket通信的进度

--- pySrc/src/analyseSong.py
+++ pySrc/src/analyseSong.py
@@ -13,11 +13,6 @@ def analyze_song(file_path, conn, lock):
         # 读取音频文件
         y, sr = librosa.load(file_path, sr=None)
         mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-        tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-
-        # 确保 tempo 是标量值
-        if isinstance(tempo, np.ndarray):
-            tempo = tempo.item()  # 提取单个元素
 
         fingerprint = np.mean(mfccs.T, axis=0)
 
@@ -29,21 +24,13 @@ def analyze_song(file_path, conn, lock):
 
         # 计算MD5哈希值
         md5_hash = hashlib.md5(arr_bytes).hexdigest()
-        message = json.dumps(
-            {
-                "md5_hash": md5_hash,
-                "file_path": file_path,
-                "bpm": round(float(tempo), 2),
-            }
-        )
+        message = json.dumps({"md5_hash": md5_hash, "file_path": file_path})
 
         # 使用锁来确保并发安全
         with lock:
             conn.send(message.encode("utf-8"))
     except Exception as e:
-        message = json.dumps(
-            {"md5_hash": "error", "file_path": file_path, "bpm": "error"}
-        )
+        message = json.dumps({"md5_hash": "error", "file_path": file_path})
 
         # 使用锁来确保并发安全
         with lock:
@@ -73,22 +60,25 @@ def main():
 
     while True:
         conn, address = frkbSocket.accept()
-        total_data = bytes()
+        total_data = b""  # 初始化总数据为空字节串
+
+        # 接收所有数据
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             total_data += data
-            if len(data) < 1000:
+            if len(data) < 1024:
                 break
+
+        # 解码并分割数据
         songArr = total_data.decode("utf-8").split("|")
+        print(songArr, flush=True)
 
         # 获取CPU核数
         num_processes = multiprocessing.cpu_count()
         # 创建一个进程池，进程数为CPU核数
-        with multiprocessing.Pool(
-            processes=num_processes, initializer=lambda: manager
-        ) as pool:
+        with multiprocessing.Pool(processes=num_processes) as pool:
             pool.starmap(
                 analyze_song, [(file_path, conn, lock) for file_path in songArr]
             )
