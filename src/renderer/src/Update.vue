88466cd10eb3644b400cd88232d9c5724f67f6f2
coderDJing <149645781+coderDJing@users.noreply.github.com>
2024-09-27 21:37:47 +0800

    升级依赖，上传update功能进度

--- src/renderer/src/Update.vue
+++ src/renderer/src/Update.vue
@@ -1,7 +1,65 @@
 <script setup>
-//todo
+import chromeMiniimize from '@renderer/assets/chrome-minimize.svg'
+import logo from '@renderer/assets/logo.png'
+import { ref } from 'vue'
+import { t } from '@renderer/utils/translate.js'
+const fillColor = ref('#9d9d9d')
+
+const toggleMinimize = () => {
+  window.electron.ipcRenderer.send('updateWindow-toggle-minimize')
+}
+
+const toggleClose = async () => {
+  window.electron.ipcRenderer.send('updateWindow-toggle-close')
+}
 </script>
-<template>132</template>
+<template>
+  <div style="height: 100%; max-height: 100%; width: 100%; display: flex; flex-direction: column">
+    <div>
+      <div class="title unselectable">{{ t('检查更新') }}</div>
+      <div class="titleComponent unselectable">
+        <div
+          style="
+            z-index: 1;
+            padding-left: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          <img :src="logo" style="width: 22px" :draggable="false" />
+        </div>
+
+        <div class="canDrag" style="flex-grow: 1; height: 35px; z-index: 1"></div>
+        <div style="display: flex; z-index: 1">
+          <div class="rightIcon" @click="toggleMinimize()">
+            <img :src="chromeMiniimize" :draggable="false" />
+          </div>
+          <div
+            class="rightIcon closeIcon"
+            @mouseover="fillColor = '#ffffff'"
+            @mouseout="fillColor = '#9d9d9d'"
+            @click="toggleClose()"
+          >
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              xmlns="http://www.w3.org/2000/svg"
+              :fill="fillColor"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss">
 #app {
@@ -9,13 +67,53 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #cccccc;
-  background-color: #313131;
+  background-color: #181818;
   width: 100vw;
   height: 100vh;
 }
 
 body {
   margin: 0px;
-  background-color: #313131;
+  background-color: #1f1f1f;
+}
+
+.title {
+  position: absolute;
+  width: 100%;
+  height: 34px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #181818;
+  z-index: 0;
+  font-size: 13px;
+  border-bottom: 1px solid #424242;
+}
+
+.titleComponent {
+  width: 100vw;
+  height: 35px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
+  box-sizing: border-box;
+
+  .rightIcon {
+    width: 47px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 35px;
+    transition: background-color 0.15s ease;
+  }
+
+  .rightIcon:hover {
+    background-color: #373737;
+  }
+
+  .closeIcon:hover {
+    background-color: #e81123;
+  }
 }
 </style>
