d66d3e74fcb13b6b8c1400d42ffc1d7ee920c92c
coderDJing <149645781+coderDJing@users.noreply.github.com>
2024-09-28 23:53:40 +0800

    完成升级功能

--- src/renderer/src/Update.vue
+++ src/renderer/src/Update.vue
@@ -9,12 +9,74 @@ const toggleMinimize = () => {
   window.electron.ipcRenderer.send('updateWindow-toggle-minimize')
 }
 
-const toggleClose = async () => {
+const toggleClose = () => {
   window.electron.ipcRenderer.send('updateWindow-toggle-close')
 }
+
+let state = ref('isRequesting')
+
+let latestVersion = ref('')
+window.electron.ipcRenderer.once('isLatestVersion', (event, version) => {
+  latestVersion.value = version
+  state.value = 'isLatest'
+})
+
+let newVersionInfo = ref({})
+window.electron.ipcRenderer.once('newVersion', (event, versionInfo) => {
+  newVersionInfo.value = versionInfo
+  state.value = 'isNewVersion'
+})
+
+window.electron.ipcRenderer.once('isError', (event) => {
+  state.value = 'isError'
+})
+
+function convertISOToCustomFormat(isoString) {
+  const date = new Date(isoString)
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
+const startDownload = () => {
+  window.electron.ipcRenderer.send('updateWindow-startDownload')
+  state.value = 'isUpdateProgress'
+}
+let progress = ref({
+  percent: 0,
+  bytesPerSecond: 0
+})
+window.electron.ipcRenderer.on('updateProgress', (event, progressObj) => {
+  progress.value = progressObj
+  state.value = 'isUpdateProgress'
+})
+
+function convertBytesToUnits(bytesPerSecond) {
+  const units = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s']
+  let unitIndex = 0
+  let convertedValue = bytesPerSecond
+
+  while (convertedValue >= 1024 && unitIndex < units.length - 1) {
+    convertedValue /= 1024
+    unitIndex++
+  }
+
+  return `${convertedValue.toFixed(2)} ${units[unitIndex]}`
+}
+
+window.electron.ipcRenderer.on('updateDownloaded', (event) => {
+  state.value = 'isUpdateDownloaded'
+})
 </script>
 <template>
-  <div style="height: 100%; max-height: 100%; width: 100%; display: flex; flex-direction: column">
+  <div
+    style="height: 100%; max-height: 100%; width: 100%; display: flex; flex-direction: column"
+    class="unselectable"
+  >
     <div>
       <div class="title unselectable">{{ t('检查更新') }}</div>
       <div class="titleComponent unselectable">
@@ -58,10 +120,129 @@ const toggleClose = async () => {
         </div>
       </div>
     </div>
+    <div
+      v-if="state === 'isRequesting'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div class="loading"></div>
+    </div>
+    <div
+      v-else-if="state === 'isLatest'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div>{{ t('没有可用的更新') }}</div>
+      <div style="margin-top: 15px">{{ t('当前版本') }} {{ latestVersion }}</div>
+    </div>
+    <div
+      v-else-if="state === 'isError'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div>{{ t('更新失败') }}</div>
+      <div
+        style="margin-top: 15px; max-width: 80%; word-wrap: break-word; overflow-wrap: break-word"
+      >
+        {{ t('可能由于网络连接问题，请检查您的网络连接或尝试使用代理服务器。') }}
+      </div>
+    </div>
+    <div
+      v-else-if="state === 'isNewVersion'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div>{{ t('发现新版本') }} {{ newVersionInfo.version }}</div>
+      <div style="margin-top: 15px">
+        {{ t('发布日期') }} {{ convertISOToCustomFormat(newVersionInfo.releaseDate) }}
+      </div>
+      <div style="margin-top: 20px; display: flex; justify-content: center">
+        <div class="button" @click="startDownload()">{{ t('开始更新') }}</div>
+      </div>
+    </div>
+    <div
+      v-else-if="state === 'isUpdateProgress'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div>{{ t('正在下载更新') }} {{ convertBytesToUnits(progress.bytesPerSecond) }}</div>
+      <div style="width: 90%; margin-top: 15px; border: 1px solid #ccc; position: relative">
+        <div
+          :style="{ width: progress.percent.toFixed(2) + '%' }"
+          style="text-align: center; height: 20px; line-height: 20px; background-color: #0078d4"
+        ></div>
+        <div
+          style="
+            text-align: center;
+            height: 20px;
+            line-height: 20px;
+            width: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+          "
+        >
+          {{ progress.percent.toFixed(2) }}%
+        </div>
+      </div>
+    </div>
+    <div
+      v-else-if="state === 'isUpdateDownloaded'"
+      style="
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      "
+    >
+      <div style="max-width: 80%; word-wrap: break-word; overflow-wrap: break-word">
+        {{ t('更新已下载，FRKB将在下次启动时自动升级') }}
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.button {
+  height: 25px;
+  line-height: 25px;
+  padding: 0 10px;
+  border-radius: 5px;
+  background-color: #2d2e2e;
+  font-size: 14px;
+
+  &:hover {
+    color: white;
+    background-color: #0078d4;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -114,6 +295,25 @@ body {
 
   .closeIcon:hover {
     background-color: #e81123;
+  }
+}
+
+.loading {
+  width: 60px;
+  height: 60px;
+  border: 5px solid #cccccc;
+  border-top-color: transparent;
+  border-radius: 100%;
+  animation: circle infinite 0.75s linear;
+}
+
+@keyframes circle {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
